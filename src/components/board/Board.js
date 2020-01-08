b5978e9140517e82e3ef9fdf14a18f231fa84c96
Joe Dei Rossi <deirossijoseph@gmail.com>
2020-01-07 18:31:56 -0800

    Created html/css structure for board and store components

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Store from '../store/Store';
 import './board.css';
 
 function Board() {
   return (
     <div className='board'>
-      content
+      <Store />
     </div>
   )
 }
---
+++ src/components/store/Store.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './store.css';
+
+function Store() {
+  return (
+    <div className='storeContainer'>
+      <div className='innerStore'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Store;
