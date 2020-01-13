127afe5dc581d1b4074254ae0e469c0e0871900a
Joe Dei Rossi <deirossijoseph@gmail.com>
2020-01-12 18:19:13 -0800

    Changed flex direction for p2 pits, might add more containers (flex?) for styling

--- src/components/pit/Pit.js
+++ src/components/pit/Pit.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './pit.css';
 
 function Pit() {
+  const [count, setCount] = React.useState(4);
+
   return (
     <div className="pit">
-      <div className="innerPit">
-        4
+      <div className="innerPit" onClick={() => console.log('pit clicked')}>
+        {count}
       </div>
     </div>
   )
