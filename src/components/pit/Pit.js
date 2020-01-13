c355067c57597a27832683aac1023b1006a03998
Joe Dei Rossi <deirossijoseph@gmail.com>
2020-01-13 12:26:18 -0800

    Converted Pit component from functional to class component

--- src/components/pit/Pit.js
+++ src/components/pit/Pit.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import './pit.css';
 
-function Pit() {
-  const [count, setCount] = React.useState(4);
+class Pit extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const selectPit = () => {
+    this.state = {
+      count: 4
+    }
+  }
+
+  selectPit = () => {
     console.log('pit clicked');
   }
 
-  return (
-    <div className="pit">
-      <div className="innerPit" onClick={selectPit}>
-        {count}
+  render() {
+    return (
+      <div className="pit">
+        <div className="innerPit" onClick={this.selectPit}>
+          {this.state.count}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Pit;
